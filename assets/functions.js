10f4a2e12606ed1f1f445e7afad002fd5e21b0a0
LuisMontanaG <l.montanagonzalez@sheffield.ac.uk>
2024-12-26 17:16:39 +0000

    Testing deployment

--- assets/functions.js
+++ assets/functions.js
@@ -2,7 +2,7 @@ window.dash_clientside = Object.assign({}, window.dash_clientside, {
     clientside: {
         get_teams_for_group: async function (database, variable) {
             try {
-                const file = "https://raw.githubusercontent.com/LuisMontanaG/BiTComparisonGraphs/refs/heads/main/GEC2017/Events.csv"
+                const file = "https://raw.githubusercontent.com/LuisMontanaG/BiTComparisonGraphs/refs/heads/main/" + database + "/Events.csv"
                 // const response = await fetch(file, {
                 //     method: 'GET',
                 //     mode: 'no-cors',
