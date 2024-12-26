054c031e3ebdc2eab94f011d761aa17cbf4416f5
LuisMontanaG <l.montanagonzalez@sheffield.ac.uk>
2024-12-26 17:07:05 +0000

    Testing deployment

--- assets/functions.js
+++ assets/functions.js
@@ -1,7 +1,23 @@
 window.dash_clientside = Object.assign({}, window.dash_clientside, {
     clientside: {
-        get_teams_for_group: function(database, variable) {
-            return database
+        get_teams_for_group: async function (database, variable) {
+            try {
+                const file = "https://raw.githubusercontent.com/LuisMontanaG/BiTComparisonGraphs/refs/heads/main/GEC2017/Events.csv"
+                // const response = await fetch(file, {
+                //     method: 'GET',
+                //     mode: 'no-cors',
+                //     headers: {
+                //         'Content-Type': 'text/csv;charset=UTF-8'
+                //     }
+                // });
+                const response = await fetch(file);
+                const data = await response.text();
+                console.log("si")
+                console.log(data)
+                return data;
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
         }
     }
 });
